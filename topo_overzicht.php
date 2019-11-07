cdf9e230d034a3929dd18484bf7fc78201db8574
2114550 <2114550@talnet.nl>
2019-11-07 09:18:23 +0100

    Uitwerking van de exercise

---
+++ topo_overzicht.php
@@ -0,0 +1,19 @@
+<?php
+$arr = array();
+echo "Hoeveel landen wil je toevoegen?".PHP_EOL;
+$input = readline();
+if (is_numeric($input)) {
+    for ($i = 0; $i < ($input); $i++)
+    {
+        echo "Hoe heet het land?".PHP_EOL;
+        $land = readline();
+        echo "Wat is de hoofdstad van $land?".PHP_EOL;
+        $hoofdstad = readline();
+        $arr[$land] = $hoofdstad;
+    }
+    echo "De landen en hoofdsteden in de database zijn:".PHP_EOL;
+    foreach ($arr as $land => $hoofdstad) {
+        echo "$land, $hoofdstad".PHP_EOL;
+
+    }
+}
